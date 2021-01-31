9ebcdbfc1f237ce934e835124c96730685fa533c
Patrique Legault <patrique.legault@gmail.com>
2021-01-31 16:35:26 -0500

    Adding initial build

---
+++ html.core/src/main/java/ioio/github/vm/patlego/App.java
@@ -0,0 +1,13 @@
+package ioio.github.vm.patlego;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
