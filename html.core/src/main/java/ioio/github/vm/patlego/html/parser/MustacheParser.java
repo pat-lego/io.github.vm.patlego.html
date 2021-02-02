47b08802da95f1b6439d1aa5e86573e6c1fbb0a8
Patrique Legault <patrique.legault@gmail.com>
2021-02-02 10:26:20 -0500

    Adding interface

--- html.core/src/main/java/ioio/github/vm/patlego/html/parser/MustacheParser.java
+++ html.core/src/main/java/ioio/github/vm/patlego/html/parser/MustacheParser.java
@@ -2,14 +2,8 @@ package ioio.github.vm.patlego.html.parser;
 
 import ioio.github.vm.patlego.html.Parseable;
 
-public class MustacheParser {
+public interface MustacheParser {
 
-    public String parse(String template, Parseable context) {
-
-    }
+    public String parse(String template, Parseable context);
     
-
-    public String parse(Template template, Parseable context) {
-        
-    }
 }
