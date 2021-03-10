f9ec156342ad28cb83ac878c8a218024ceb2e95f
pat-lego <patrique.legault@gmail.com>
2021-03-09 20:51:16 -0500

    Adding LocalDateTime adapter

--- html.core/src/main/java/io/github/vm/patlego/html/core/components/SimpleComponentManager.java
+++ html.core/src/main/java/io/github/vm/patlego/html/core/components/SimpleComponentManager.java
@@ -3,13 +3,25 @@ package io.github.vm.patlego.html.core.components;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import io.github.vm.patlego.html.core.utils.GsonUtil;
+import io.github.vm.patlego.html.core.utils.impl.ComponentGsonUtil;
 import io.github.vm.patlego.html.datasource.repo.ComponentDS;
 
 @Component(service = ComponentManager.class, immediate = true)
@@ -21,13 +33,15 @@ public class SimpleComponentManager implements ComponentManager {
     @Override
     public io.github.vm.patlego.html.datasource.tables.Component createComponent(InputStream in) throws IOException {
 
-        io.github.vm.patlego.html.datasource.tables.Component component = new Gson().fromJson(new InputStreamReader(in),
+        Gson gson = new ComponentGsonUtil().getGson();
+        
+        io.github.vm.patlego.html.datasource.tables.Component component = gson.fromJson(new InputStreamReader(in),
                 io.github.vm.patlego.html.datasource.tables.Component.class);
 
         if (component.getCreated() == null) {
             component.setCreated(LocalDateTime.now());
         }
-        
+
         if (component.getUpdated() == null) {
             component.setUpdated(LocalDateTime.now());
         }
---
+++ html.core/src/main/java/io/github/vm/patlego/html/core/utils/GsonUtil.java
@@ -0,0 +1,8 @@
+package io.github.vm.patlego.html.core.utils;
+
+import com.google.gson.Gson;
+
+public interface GsonUtil {
+    
+    public Gson getGson();
+}
